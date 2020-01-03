3fbc5e1151a925963faebb3f4d9cfeb656c28fd7
ViggieSmalls <victormovileanu@gmail.com>
2020-01-03 11:43:29 +0100

    initial commit - npm setup - django installation

---
+++ LinkShare/urls.py
@@ -0,0 +1,32 @@
+"""LinkShare URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+# todo
+#  create home page
+#   add svg icons
+#   create top nav
+#   create sidebar with links
+#   create search bar (copy from trillo project)
+#  create categories page
+#   add 6 sample categories
+#   add jscroll or whatever for scroling through the links
+#   add search inside each category image on hover/click
